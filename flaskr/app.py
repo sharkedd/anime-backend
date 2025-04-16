631ac6a753fdfece3c05c6bcef2a34247527c3a2
jose miguel carrillo rojas <jose.carrillo@alumnos.ucn.cl>
2025-04-16 00:16:26 -0400

    Inicialización mongo

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,7 +1,16 @@
 from flask import Flask
 from flask_graphql import GraphQLView
 from .schemas.schema import schema
+from mongoengine import *
+import os
 app = Flask(__name__)
+
+
+try:
+    connect(host=os.getenv('MONGO_URI'))
+except Exception as e:
+    print(f"Error al conectar a MongoDB: {e}")
+
 
 app.add_url_rule(
     '/graphql',
--- flaskr/routes/__init__.py
+++ flaskr/routes/__init__.py
@@ -1,7 +1,5 @@
 # routes/__init__.py
-from .db_routes import db_bp
 from .jikan_api_routes import jikan_bp
 
 def register_routes(app):
-    app.register_blueprint(db_bp, url_prefix='/db')
     app.register_blueprint(jikan_bp, url_prefix='/anime')
