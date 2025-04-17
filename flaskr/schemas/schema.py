afaf781be346cf99bd235f7bf0a8feaa1a6fcf63
jose miguel carrillo rojas <jose.carrillo@alumnos.ucn.cl>
2025-04-17 11:52:59 -0400

    Creación modelo de usuario y mutación para crearlo

--- flaskr/schemas/schema.py
+++ flaskr/schemas/schema.py
@@ -1,6 +1,8 @@
 import graphene
 from .anime.queries import AnimeQueries # Importa las consultas de anime
 from .anime.mutations import SaveAnime  # Importa la mutación para guardar anime
+from .user.mutations import CreateUser  # Importa la mutación para crear usuario
+
 
 class Query(
     AnimeQueries,  # Importa las consultas de anime
@@ -10,6 +12,7 @@ class Query(
 
 class Mutation(graphene.ObjectType): # Clase base de Graphene para definir el esquema)
     SaveAnime = SaveAnime.Field()  # Define la mutación para guardar anime
+    CreateUser = CreateUser.Field()  # Define la mutación para crear usuario
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)  # Crea el esquema de GraphQL con las consultas y mutaciones definidas
