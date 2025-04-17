f3832a8a75f24c070d0b6b1934443abc0ccde67a
jose miguel carrillo rojas <jose.carrillo@alumnos.ucn.cl>
2025-04-17 09:56:07 -0400

    Modificación mutación para guardar anime favorito

--- flaskr/schemas/schema.py
+++ flaskr/schemas/schema.py
@@ -1,5 +1,6 @@
 import graphene
-from .anime.queries import AnimeQueries
+from .anime.queries import AnimeQueries # Importa las consultas de anime
+from .anime.mutations import SaveAnime  # Importa la mutación para guardar anime
 
 class Query(
     AnimeQueries,  # Importa las consultas de anime
@@ -7,4 +8,8 @@ class Query(
 ):
     pass
 
-schema = graphene.Schema(query=Query)
+class Mutation(graphene.ObjectType): # Clase base de Graphene para definir el esquema)
+    SaveAnime = SaveAnime.Field()  # Define la mutación para guardar anime
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)  # Crea el esquema de GraphQL con las consultas y mutaciones definidas
